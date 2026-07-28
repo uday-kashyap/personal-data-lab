4134a12dfa8859271b57341b2f7727dec42a8333
kashyapuday <kashyapuday063@gmail.com>
2026-07-28 17:25:41 +0530

    refactor: establish CLI output format consistency across record viewing

--- src/presentation.py
+++ src/presentation.py
@@ -6,11 +6,14 @@ def present_record(record: Record) -> None:
     Display given record into a user-readable manner.
     """
 
-    print("----Your Record----")
+    print("========== Your Record ==========")
+
     for field, field_val in record.items():
         cleaned_field = field.replace("_", " ")
 
         print(f"{cleaned_field.title()}: {field_val}")
+
+    print("=================================")
 
 
 def display_menu(menu_options: dict[int, str]) -> None:
